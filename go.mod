02821e406e17e165539935c20000adb8000315df
Rami2212 <ramithapathmilarp@gmail.com>
2025-12-06 17:11:13 +0530

    add handlers

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.25.5
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.10
+	github.com/redis/go-redis/v9 v9.17.2
 	gorm.io/gorm v1.31.1
 )
 
@@ -23,7 +24,6 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
-	github.com/redis/go-redis/v9 v9.17.2 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
---
+++ internal/handlers/url_handler.go
@@ -0,0 +1,73 @@
+package handlers
+
+import (
+	"net/url"
+
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/Rami2212/Url-Shortener-Backend-Go/internal/config"
+	"github.com/Rami2212/Url-Shortener-Backend-Go/internal/services"
+	"github.com/Rami2212/Url-Shortener-Backend-Go/internal/utils"
+)
+
+type URLHandler struct {
+	service *services.URLService
+	cfg     *config.Config
+}
+
+func NewURLHandler(service *services.URLService, cfg *config.Config) *URLHandler {
+	return &URLHandler{
+		service: service,
+		cfg:     cfg,
+	}
+}
+
+type shortenRequest struct {
+	URL string `json:"url"`
+}
+
+func (h *URLHandler) Shorten(c *fiber.Ctx) error {
+	var body shortenRequest
+
+	if err := c.BodyParser(&body); err != nil {
+		return utils.JSONError(c, fiber.StatusBadRequest, "Invalid request body")
+	}
+
+	if body.URL == "" {
+		return utils.JSONError(c, fiber.StatusBadRequest, "URL is required")
+	}
+
+	// Basic URL validation
+	if _, err := url.ParseRequestURI(body.URL); err != nil {
+		return utils.JSONError(c, fiber.StatusBadRequest, "Invalid URL format")
+	}
+
+	shortURL, code, err := h.service.Shorten(body.URL)
+	if err != nil {
+		return utils.JSONError(c, fiber.StatusInternalServerError, "Failed to shorten URL")
+	}
+
+	resp := fiber.Map{
+		"short_url":    shortURL,
+		"short_code":   code,
+		"original_url": body.URL,
+	}
+
+	return utils.JSONSuccess(c, resp)
+}
+
+// GET /:code
+func (h *URLHandler) Redirect(c *fiber.Ctx) error {
+	code := c.Params("code")
+	if code == "" {
+		return utils.JSONError(c, fiber.StatusBadRequest, "Short code is required")
+	}
+
+	originalURL, err := h.service.ResolveShortCode(code)
+	if err != nil {
+		return utils.JSONError(c, fiber.StatusNotFound, "Short URL not found")
+	}
+
+	// 307 keeps the method (POST stays POST), but for simple GET a 302 is also fine
+	return c.Redirect(originalURL, fiber.StatusTemporaryRedirect)
+}
